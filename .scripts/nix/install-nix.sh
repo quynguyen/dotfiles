46cb6c5ed80dc8eeea91ba9e381ed3c58e2f6f25
Quy Nguyen <quy.nguyen@gmail.com>
2023-01-09 15:42:13 -0800

    Updated the Nix install script to explicitly use the most updated channel and packages

--- .scripts/nix/install-nix.sh
+++ .scripts/nix/install-nix.sh
@@ -5,9 +5,10 @@ set -e
 # install nix (if not already installed)
 if [[ ! -f ~/.nix-profile/etc/profile.d/nix.sh ]]; then
 	sh <(curl -L https://nixos.org/nix/install) --no-daemon
-else
-	nix-channel --update && nix-env -u
 fi
 
 # Source Nix
 source ~/.nix-profile/etc/profile.d/nix.sh
+
+# Use the most update-to-date packages
+nix-channel --update && nix-env -u
