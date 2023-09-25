6c9d27761b3a7aae60b13bf505977d1e954bbdd8
Quy Nguyen <quy.nguyen@gmail.com>
2023-09-25 10:45:25 -0700

    Made it work for spin

--- .scripts/zsh/generate-home-zshrc.sh
+++ .scripts/zsh/generate-home-zshrc.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 
 # Move any existing .zshrc
-if [[ -f ~/.zshrc ]]; then
+if [[ -f ~/.zshrc ]] && [[ -z ~/.zshrc.old ]]; then
 	mv ~/.zshrc ~/.zshrc.old
 fi
 
 # Go two parent dirs up from this script
-pushd "$( dirname ${BASH_SOURCE:-$0} )/../../" > /dev/null
+pushd "$(dirname ${BASH_SOURCE:-$0})/../../" >/dev/null
 
+export REPLACE_spin='${SPIN}'
 export REPLACE_source=${BASH_SOURCE:-$0}
 export REPLACE_dotfiles_path=$(pwd)
 export REPLACE_home=$HOME
 export REPLACE_file='$file'
 export REPLACE_p10k_home='${XDG_CACHE_HOME:-$HOME/.cache}'
 export REPLACE_p10k_prompt='${(%):-%n}'
-export REPLACE_warning=$(cat <<-END
-# !!!!! WARNING !!!!! 
-#
-# This file is generated. To make changes to the source...
-# 
-# Option 1:
-#	nvim ~/.zshrc_template
-#	run '${REPLACE_source}
-# 
-# Option 2:
-#	Run the convenience alias 'nz'
-#
-END
+export REPLACE_warning=$(
+	cat <<-END
+		# !!!!! WARNING !!!!! 
+		#
+		# This file is generated. To make changes to the source...
+		# 
+		# Option 1:
+		# nvim ~/.zshrc_template
+		# run ${REPLACE_source}
+		# 
+		# Option 2:
+		# Run the convenience alias 'nz'
+		#
+	END
 )
 
 # Go back
-popd  > /dev/null
+popd >/dev/null
 
 # Generate a .zshrc from a template, REPLACING in ${USER}-specific values
-envsubst < ~/.zshrc_template > ~/.zshrc
+envsubst <~/.zshrc_template >~/.zshrc
 
 # for interactive shells
 if [[ $- == *i* ]]; then
--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # go there the directory of this currently 'sourced' script ( quietly )
-pushd $( dirname ${BASH_SOURCE:-$0} ) > /dev/null
+pushd $(dirname ${BASH_SOURCE:-$0}) >/dev/null
 
-# Nix
-source .scripts/nix/install-nix.sh
+if [[ -z "$SPIN" ]]; then
+	source .scripts/nix/install-nix.sh
+else
+	echo "********************************************************************************"
+	echo "This is a SPIN environment"
+	echo "Nix is already installed"
+	echo "********************************************************************************"
+fi
 
 # Nix Packages
 source .scripts/nix/install-nix-packages.sh
@@ -18,14 +24,14 @@ source .scripts/antibody/generated-zsh-plugins.sh
 # ~/.zshrc
 source .scripts/zsh/generate-home-zshrc.sh
 
+# Reload zsh
+exec zsh
+
 # Tmux Plugins
 source .scripts/tmux/install-tmux-plugins.sh
 
 # Neovim plugins
 source .scripts/nvim/install-nvim-plugins.sh
 
-# Reload zsh
-exec zsh
-
 # go back to working directory ( quietly )
-popd  > /dev/null
+popd >/dev/null
