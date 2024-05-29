509d872a1ced02d1820ca7c2ad427e978a6cb6c3
Quy Nguyen <quy.nguyen@gmail.com>
2024-05-28 21:14:34 -0700

    Force Commit

--- .stow-packages/nvim/.config/nvim-lazyvim/lua/plugins/copilot.lua
+++ .stow-packages/nvim/.config/nvim-lazyvim/lua/plugins/copilot.lua
@@ -12,17 +12,4 @@ return {
       },
     },
   },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = true },
-      panel = { enabled = true },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
-  },
 }
