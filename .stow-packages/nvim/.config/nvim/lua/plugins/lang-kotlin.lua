0ee80f9388e576020cbc8b1b7919107ef139abf2
Quy Nguyen <quy.nguyen@gmail.com>
2026-03-13 14:57:58 -0700

    feat: kotlin-language-server + ktlint

---
+++ .stow-packages/nvim/.config/nvim/lua/plugins/lang-kotlin.lua
@@ -0,0 +1,28 @@
+-- Kotlin: kotlin-language-server (via Mason) + ktlint formatter/linter.
+-- LazyVim v15 has no lang.kotlin extra; this spec provides full coverage.
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        kotlin_language_server = {},
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        kotlin = { "ktlint" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        kotlin = { "ktlint" },
+      },
+    },
+  },
+}
